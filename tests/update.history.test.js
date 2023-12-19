217a9e5b55ede9f333a45a7338d87fbe7a5a5505
ruymaster <mercury1984@outlook.com>
2023-12-19 14:16:17 +0000

    Pushing the previous dev jobs

--- tests/update.history.test.js
+++ tests/update.history.test.js
@@ -6,6 +6,8 @@ const { PPVTransaction } = require('../models/PPVTransaction');
 const { WatchHistory } = require('../models/WatchHistory');
 const { sleep } = require('../utils/time');
 const { publicChatChannelId } = require('../config/constants');
+const { getStakedAmountOfAddresses } = require('../utils/web3');
+const { Balance } = require('../models/Balance');
 
 mongoose.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.dbName,
     { useNewUrlParser: true, useUnifiedTopology: true }, async function (err, db) {
@@ -22,6 +24,17 @@ mongoose.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/
     });
 
 async function cron_loop() {
+    const addresses = await Balance.find({ address: { $ne: null } }, { address: 1 }).distinct('address');
+    console.log('--addresses', addresses.length);
+    const stakedResult = await getStakedAmountOfAddresses(addresses);
+    console.log('--get all staked amounts');
+    for (let i = 0; i < addresses.length; i++) {
+        const address = addresses[i];
+        if (!address || address === '0x0000000000000000000000000000000000000000') continue;        
+        await Balance.updateOne({ address }, { staked: Number(stakedResult[address]) });
+    }
+    console.log('--done');
+    return;
     const pubnub = new PubNub({
         publishKey: process.env.PUBNUB_PUBKEY,
         subscribeKey: process.env.PUBNUB_SUBKEY,
