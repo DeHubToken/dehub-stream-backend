73f1c7be82aab04e952c7f21c2f6a4ddf2cf817a
Alhaji <alhaji4megahit@gmail.com>
2023-12-22 10:28:32 +0100

    Checking the flow

--- routes/home_route.js
+++ routes/home_route.js
@@ -5,7 +5,11 @@ let middleware_controller = require('../controllers/MiddlewareController');
 let home_controller = require('../controllers/HomeController');
 
 router.get('/', middleware_controller.m_checkLogin, function (req, res, next) {
-    return "Not Call !!!";
+  return 'Not Call !!!';
+});
+
+router.get('/dehub', (req, res, next) => {
+  res.json({ message: 'Welcome to Dehub Backend server' });
 });
 
 module.exports = router;
